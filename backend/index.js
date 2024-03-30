bd6f88423610ab7baf4117513f3628e1e9d1ca7b
Simran Jain <jain573simran@gmail.com>
2024-03-30 23:02:11 +0530

    9th Commit by Simran

--- backend/index.js
+++ backend/index.js
@@ -16,15 +16,15 @@ const port =  process.env.PORT;
 const connectToDB  = require("./db");
 connectToDB();
 
-// middleware 
-app.use((req,res,next) => {
-    res.setHeader("Access-Control-Allow-Origin" , "http://localhost:3000");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin , X-Requested-With , Content-Type , Accept"
-    );
-    next();
-})
+// // middleware 
+// app.use((req,res,next) => {
+//     res.setHeader("Access-Control-Allow-Origin" , "http://localhost:3000");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin , X-Requested-With , Content-Type , Accept"
+//     );
+//     next();
+// })
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
