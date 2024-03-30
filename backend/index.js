aabf21eb7fbe7706e0a27538f87a5fc823c97811
Simran Jain <jain573simran@gmail.com>
2024-03-30 22:18:46 +0530

    Fifth Commit by Simran

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const cors = require('cors');
 app.use(cors(
     { 
-        origin: [""],
+        origin: ["https://bite-buddy-frontend.vercel.app"],
         methods: ['GET', 'POST'],
         credentials : true
     }
